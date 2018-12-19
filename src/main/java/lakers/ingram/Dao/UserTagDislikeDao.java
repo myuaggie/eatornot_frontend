dac72363d9d73c8ca782b96e2e5c5966959f2123
myuaggie <920369216@qq.com>
2018-12-19 22:24:33 +0800

    update

--- src/main/java/lakers/ingram/Dao/UserTagDislikeDao.java
+++
@@ -1,9 +0,0 @@
-package lakers.ingram.Dao;
-
-import net.sf.json.JSONArray;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserTagDislikeDao {
-    public String updateUserDislikeTag(Integer userId,JSONArray dislikeTags);
-}
--- src/main/java/lakers/ingram/OSUtil/OSUtil.java
+++
@@ -1,8 +0,0 @@
-package lakers.ingram.OSUtil;
-
-public class OSUtil {
-
-    public static String getOS(){
-        return System.getProperties().getProperty("os.name");
-    }
-}
